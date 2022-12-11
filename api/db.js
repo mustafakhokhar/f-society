6a39995a29222df6c6c1dd5e3aa7048f031bde9d
Mustafa Amir Khokhar <mustafakhokhar26@gmail.com>
2022-12-12 00:50:04 +0500

    finalisoed

--- api/db.js
+++ api/db.js
@@ -1,16 +1,3 @@
 import mysql from "mysql"
-import dbConfig from "./db_config.js";
 
-// export const db = mysql.createConnection({
-//   host:"us-cdbr-east-06.cleardb.net",
-//   user:"b427d8cf20ccbb",
-//   password: "7d30cb42",
-//   database:"heroku_ecfadd55d5194b2",
-// })
-
-export var connection = mysql.createPool({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB
-});
+export const db = mysql.createPool('mysql://b427d8cf20ccbb:7d30cb42@us-cdbr-east-06.cleardb.net/heroku_ecfadd55d5194b2?reconnect=true')
